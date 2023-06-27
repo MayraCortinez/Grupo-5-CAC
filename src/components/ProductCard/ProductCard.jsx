af6ad090c2cf340037eb82d939a42cee81d479e3
Jose Aragon <115207491+mrlillo461@users.noreply.github.com>
2023-06-27 20:34:47 -0300

    Add ProductCard structure
    Styling card adding ProductCard.css file

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+// import React from "react";
 import { Link } from "react-router-dom";
+import "./ProductCard.css"
+import AirMax from "./ID1_Air_Max_1.png";  
 
-function ProductCard({ product }) {
+function ProductCard() {
   return (
-    <div>
-      <h3>{product.name}</h3>
-      <p>Price: ${product.price}</p>
-      <Link to={`/product/${product.id}`}>View Details</Link>
+    <div className="container">
+      <div className="card">
+        <div className="card-img">
+          <img src={ AirMax } alt="product.modelo" />
+        </div>
+        <div className="card-content">
+         <h2 className="card-title">Air Max 1</h2>
+         <div className="card-details">
+           <div className="size">
+             <h3>Size :</h3>
+             <span>42</span>
+           </div>
+           <div className="color">
+             <h3>Color :</h3>
+            <span></span>
+          </div>
+          <div className="price">
+            <h3>$ 35.500</h3>
+          </div>
+         </div>
+         <Link to={`/product/1`}>More Details</Link>
+       </div>
+      </div>
     </div>
   );
 }
