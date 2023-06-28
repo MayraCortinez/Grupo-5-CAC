0e91c85ce6628c444e9603bd9a40b5cb0bb08ed4
MayC <mayrautopia@gmail.com>
2023-06-27 22:54:22 -0300

    fix folder & delete productDetail component

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 function ProductCard({ product }) {
   return (
     <div>
-      <h3>{product.name}</h3>
+{/*       <h3>{product.name}</h3>
       <p>Price: ${product.price}</p>
-      <Link to={`/product/${product.id}`}>View Details</Link>
+      <Link to={`/product/${product.id}`}>View Details</Link> */}
     </div>
   );
 }
--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import ProductCard from '../../components/ProductCard/ProductCard'
 
-function ProductsDetails() {
+function ProductDetail() {
   const { id } = useParams();
 
   return (
     <div>
       <h2>Product Details</h2>
       <p>Product ID: {id}</p>
+      <ProductCard />
       {/* Fetch product details from API or use local data */}
     </div>
   );
 }
 
-export default ProductsDetails;
+export default ProductDetail;
