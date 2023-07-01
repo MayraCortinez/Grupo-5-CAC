8a02b7d9210f0cb8e6e7468881890965f36b348c
jose del valle <josedelvalle012@gmail.com>
2023-06-30 21:05:44 -0300

    Se agrego componente Login

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Lógica de inicio de sesión
+  };
+
+  return (
+    <div className="container">
+      <h2>Login</h2>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
