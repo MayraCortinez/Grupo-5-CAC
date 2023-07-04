339c8b3ef67f4db47875106feeb152efaccf0db3
jose del valle <josedelvalle012@gmail.com>
2023-07-04 00:14:36 -0300

    Add FormContact

---
+++ src/components/FormContact/FormContact.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+const FormContact = () => {
+  return (
+    <div>
+      <h2 style={{ color: "#fff", textAlign: "center", margin: "10px"}}>Contacto</h2>
+      <Form action="reactjsgrupo5@gmail.com" method="POST" style={{ width: "45%", margin: "0 auto"}}>
+        <Form.Group controlId="name" style={{ margin:"10px 0"}}>
+          <Form.Label style={{ color: "#fff", textAlign: "center"}}>Nombre</Form.Label>
+          <Form.Control type="text" name="name" required />
+        </Form.Group>
+        <Form.Group controlId="email" style={{ margin:"10px 0"}}>
+          <Form.Label style={{ color: "#fff", textAlign: "center"}}>Email</Form.Label>
+          <Form.Control type="email" name="email" required />
+        </Form.Group>
+        <Button variant="primary" type="submit" style={{ margin:"10px 0"}}>
+          Enviar
+        </Button>
+      </Form>
+    </div>  
+  );
+};
+
+export default FormContact;
