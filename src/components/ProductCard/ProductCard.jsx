1f0ff2ada2f285d4fc163cb7796f160746dd906f
MayC <mayrautopia@gmail.com>
2023-07-05 02:15:33 -0300

    ProductList with error

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,34 +1,31 @@
-// import React from "react";
 import { Link } from "react-router-dom";
-import "./ProductCard.css"
-import AirMax from "./ID1_Air_Max_1.png";  
+import "./ProductCard.css";
 
-function ProductCard(  { modelo, talle, color, precio, img } ) {
+function ProductCard({ modelo, talle, color, precio, img}) {
   return (
-
     <div className="my-container">
       <Link to={`/cart`}>
-      <div className="my-card">
-        <div className="my-card-img">
-          <img src={ img } alt={img} />
+        <div className="my-card">
+          <div className="my-card-img">
+            <img src={img} alt={img} />
+          </div>
+          <div className="my-card-content">
+            <h2 className="my-card-title">{modelo}</h2>
+            <div className="my-card-details">
+              <div className="my-size">
+                <h3>Size :</h3>
+                <span>{talle}</span>
+              </div>
+              <div className="my-color">
+                <h3>Color :</h3>
+                <span>{color}</span>
+              </div>
+              <div className="my-price">
+                <h3>${precio}</h3>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="my-card-content">
-         <h2 className="my-card-title">{modelo}</h2>
-         <div className="my-card-details">
-           <div className="my-size">
-             <h3>Size :</h3>
-             <span>{talle}</span>
-           </div>
-           <div className="my-color">
-             <h3>Color :</h3>
-            <span>{color}</span>
-          </div>
-          <div className="my-price">
-            <h3>${precio}</h3>
-          </div>
-         </div>
-       </div>
-      </div>
       </Link>
     </div>
   );
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,22 +1,22 @@
-import { useState, useEffect } from "react";
-import ProductCard from "../ProductCard/ProductCard";
-import { collection, getDocs } from 'firebase/firestore';
+import React, { useState, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
-import { db } from '../../firebaseConfig/firebase';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { db, gsReference } from "../../firebaseConfig/firebase";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import ProductCard from "../ProductCard/ProductCard";
 
 function ProductList() {
   const [productos, setProductos] = useState([]);
 
-  const productosCollection = collection(db, "productos");
-
   const getProductos = async () => {
-    const data = await getDocs(productosCollection);
-    setProductos(
-      data.docs.map(doc => ({ ...doc.data(), id: doc.id }))
-    );
+    const querySnapshot = await getDocs(collection(db, "productos"));
+    const productosData = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    setProductos(productosData);
   };
 
   useEffect(() => {
@@ -25,30 +25,29 @@ function ProductList() {
 
   const storage = getStorage();
 
-  const getUrl = async (img) => {
-    const storageRef = ref(storage, img);
-    const urlImg = await getDownloadURL(storageRef);
-    return urlImg;
+  const fetchImageUrls = async () => {
+    const updatedProductos = await Promise.all(
+      productos.map(async (producto) => {
+        const imageUrl = await getDownloadURL(
+          ref(storage, `${gsReference}/${producto.img}`)
+        );
+        return {
+          ...producto,
+          img: imageUrl,
+        };
+      })
+    );
+    setProductos(updatedProductos);
   };
 
   useEffect(() => {
-    const fetchImageUrls = async () => {
-      const updatedProductos = await Promise.all(
-        productos.map(async (producto) => ({
-          ...producto,
-          img: await getUrl(`images/${producto.img}`),
-        }))
-      );
-      setProductos(updatedProductos);
-    };
-
     fetchImageUrls();
   }, [productos]);
 
   return (
     <Container fluid className="mt-3">
       <Row className="pt-5">
-        {productos?.map((producto) => (
+        {productos.map((producto) => (
           <Col key={producto.id} className="pt-5 px-3">
             <ProductCard
               modelo={producto.modelo}
