a2a20aaf82e4aa30582d5f423713661188e16947
Jose Aragon <115207491+mrlillo461@users.noreply.github.com>
2023-07-10 17:21:12 -0300

    Add text to navbar logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,23 @@
 import logo from "../../assets/3.png"
-
+import "./Header.css"
 //  Navbar, Nav y Container de React Bootstrap
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap';
 
 // Uso tema oscuro (bg="dark") y el texto blanco (style={{ color: 'white' }}) para el header
 const Header = () => {
   return (
     <Navbar bg="dark" data-bs-theme="dark" expand="lg" fixed="top">
       <Container fluid className='px-5'>
-        <Navbar.Brand href="/">
-          <img src={ logo } alt="VZU" width="80"/>
-        </Navbar.Brand>
+        <Row>
+          <Navbar.Brand href="/" className="d-flex align-items-center justify-content-center custum-navbar-brand">
+            <Col>
+              <img src={ logo } alt="VZU" width="80"/>
+            </Col>
+            <Col>
+              <span className="fs-2">Venta de Zapatillas</span>
+            </Col>
+          </Navbar.Brand>
+        </Row>
         <Navbar.Toggle aria-controls="navbar" />
         <Navbar.Collapse className='align-items-center justify-content-end' id="navbar">
           <Nav>
